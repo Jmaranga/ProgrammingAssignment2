c00115e91e210ed6d934ac667367dc2c38cc5c48
Jared Maranga <74311162+Jmaranga@users.noreply.github.com>
2020-12-09 15:19:42 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){ ## I assume the matrix to supply here will be invertible
+  bn <- NULL ## creating a null variable to the inverse
+  set <- function(y){ ## Setting the value of the Matrix using a different/another function
+    x <<- y ## Creating a function returned by another function using <<-
+    bn <<- NULL
+  }
+  get <- function() {x}    ## Getting the value of the Matrix
+  setInverse <- function(inverse) {bn <<- inverse} ## Setting the value of the Inverse and assign it to setInverse
+  getInverse <- function() {bn} ## Getting the value of the inverse
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse) ## Creating a list that will hold the inverse
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){ ## cacheSolve is a function which computes the inverse of the special "matrix"
+  bn <- x$getInverse() ## Returning a matrix that is inverse of x and assigns it to bn
+  if(!is.null(bn)){ ## Checking if the inverse has been generated and if so, avoid further computation
+    message("getting cached data") ## if the inverse will be retrieved from the Cache, this will be displayed
+    return(bn) ## The inverse will be returned
+  } ## Otherwise, Computing the values of the Matric
+  mat <- x$get() 
+  bn <- solve(mat, ...) ## Computing the inverse of the matrix
+  x$setInverse(bn) ## Set the value of the inverse in Cache by sung setInverse function
+  bn ## Return the inverse
 }
